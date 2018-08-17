a16248619a11bf5b91f76d82e764a5823a5c1bfe
psycorax <reisinger.thre@gmail.com>
2018-08-17 22:08:15 +0200

    added all relevant files for the bac

---
+++ init.el
@@ -0,0 +1,19 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(add-to-list 'package-archives
+	     '("org" . "https://orgmode.org/elpa/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(use-package org
+       :ensure org-plus-contrib
+       :pin org)
+
+(org-babel-load-file (expand-file-name "~/Documents/git/emacs/myinit_coding.org"))
